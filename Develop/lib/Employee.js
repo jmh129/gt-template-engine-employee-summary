bb7146fbf9f6868ac35a0041c7d9ba2fa68b0ec5
James Holding <james.holding90@gmail.com>
2020-06-30 13:34:08 -0400

    added employee classes.

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,25 @@
 // TODO: Write code to define and export the Employee class
+
+class Employee {
+  constructor(name, id, email) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+  }
+  getName() {
+    console.log(this.name);
+    return this.name;
+  }
+  getId() {
+    console.log(this.id);
+    return this.id;
+  }
+  getEmail() {
+    console.log(this.email);
+    return this.email;
+  }
+  getRole() {
+    console.log(this.role);
+    return "Employee";
+  }
+}
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,15 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Manager extends Employee {
+  constructor(name, id, email) {
+    super(name, id, email);
+    this.officeNumber = this.officeNumber;
+  }
+  getRole() {
+    return "Manager";
+  };
+  getOfficeNumber() {
+    return this.officeNumber;
+  };
+}
